8fe1ac7323a12b7af5b976ac4137feeb90ca2782
n-olivier <onshimiyimana17@alustudent.com>
2019-11-24 00:32:59 +0200

    initial commit

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const app = firebase.app();
+    console.log(app);
+
+});
+
+function signIn(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+
+}
